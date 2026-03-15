ec9f67b4621e05c0ce32e9090a3b5ce7a4762437
gutek28 <jakubgutowski@cyfroweszkoly.pl>
2026-03-15 22:40:58 +0100

    Payment.tsx Dodanie właściwego tekstu

--- src/Payment.tsx
+++ src/Payment.tsx
@@ -1,1 +1,25 @@
 
+import './Payment.css'
+
+function Payment() {
+    return (
+        <div className="Div">
+            <header className="Header">
+                <h1>💳 Strona płatności</h1>
+                <p>Ta strona jest w budowie...</p>
+            </header>
+
+            <h2>Podsumowanie rezerwacji:</h2>
+
+            <p>Imię: ....................</p>
+            <p>Sprzęt: ....................</p>
+            <p>Godziny: ....................</p>
+            <p>Kapok: Tak / Nie</p>
+            <p>Instruktor: Tak / Nie</p>
+
+            <h2>Do zapłaty: .......... zł</h2>
+        </div>
+    );
+}
+
+export default Payment;
