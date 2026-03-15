a973b70bc69ffb103db5759b2a98d34b6948fa3e
gutek28 <jakubgutowski@cyfroweszkoly.pl>
2026-03-15 22:37:37 +0100

    Dodanie wyglądu

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className="Div">
+            <h1>⚓Mazurska Przystań - Rezerwacja⛵</h1>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <form className="formularz">
+                <p>Imię klienta:</p>
+                <input
+                    name="imie"
+                    type="text"
+                    value=""
+                    onChange={() => {}}
+                    required
+                />
+
+                <p>Wybierz sprzęt:</p>
+
+                <select
+                    name="sprzet"
+                    value="kajak"
+                    onChange={() => {}}
+                >
+                    <option value="kajak">Kajak (20 zł/h)</option>
+                    <option value="rower">Rower wodny (35 zł/h)</option>
+                    <option value="omega">Omega (150 zł/h)</option>
+                </select>
+                <p>Ile godzin (1-8):</p>
+                <input
+                    name="godziny"
+                    type="range"
+                    min="1"
+                    max="8"
+                    value="1"
+                    onChange={() => {}}
+                />
+                <p>Wybrano: 1 h</p>
+                <div className="dodatki">
+                    <label>
+                        <input
+                            name="kapok"
+                            type="checkbox"
+                            checked={false}
+                            onChange={() => {}}
+                        />
+                        Kapok (+5 zł)
+                    </label>
+                    <label>
+                        <input
+                            name="instruktor"
+                            type="checkbox"
+                            checked={false}
+                            onChange={() => {}}
+                        />
+                        Instruktor (+50 zł/h)
+                    </label>
+                </div>
+                <label className="regulamin">
+                    <input name="regulamin" type="checkbox" required />
+                    Akceptuję regulamin
+                </label>
+
+                <button type="submit">Przejdź do płatności</button>
+            </form>
+        </div>
+    );
 }
 
-export default App
+export default App;
